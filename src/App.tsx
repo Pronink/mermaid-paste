5d1052837155ecc4faa64781f25d1cea090b40c1
pronink <ismael.garcia.torres@gmail.com>
2026-05-31 02:03:48 +0200

    Replace LZString with Pako for compression, enhance error handling, modularize compression logic, and update global styles and dependencies.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import { MermaidViewer } from './MermaidViewer.tsx'
 import { DraggableZoom } from './DraggableDiv.tsx'
 import { useEffect, useState } from 'react'
-import LZString from 'lz-string'
 import styles from './App.module.css'
 import { Editor } from './Editor.tsx'
 import mermaid from 'mermaid'
+import { compressToUrl, decompressFromUrl } from './compression.ts'
 
 mermaid.initialize({
   startOnLoad: false,
@@ -14,37 +14,43 @@ mermaid.initialize({
 export const App = () => {
   const [mermaidCode, setMermaidCode] = useState<string>('')
   const [isEditorVisible, setIsEditorVisible] = useState(false)
+  const [errorMessage, setErrorMessage] = useState<string>()
 
   // Load URL at startup
   useEffect(() => {
     const params = new URLSearchParams(window.location.search)
     const compressed = params.get('m')
+
     if (compressed) {
-      const decompressed =
-        LZString.decompressFromEncodedURIComponent(compressed)
+      const decompressed = decompressFromUrl(compressed)
+
       if (decompressed) {
         setMermaidCode(decompressed)
         setIsEditorVisible(false)
         return
       }
     }
+
     setIsEditorVisible(true)
   }, [])
 
   // Update URL
   useEffect(() => {
     if (!isEditorVisible) return
+
     let newUrl = window.location.origin
+
     if (mermaidCode) {
-      newUrl += `?m=${LZString.compressToEncodedURIComponent(mermaidCode)}`
+      newUrl += `?m=${compressToUrl(mermaidCode)}`
     }
+
     window.history.replaceState(null, '', newUrl)
   }, [mermaidCode, isEditorVisible])
 
   return (
     <div className={styles.root}>
       <DraggableZoom>
-        <MermaidViewer mermaidCode={mermaidCode} />
+        <MermaidViewer mermaidCode={mermaidCode} onError={setErrorMessage} />
       </DraggableZoom>
       <Editor
         code={mermaidCode}
@@ -54,6 +60,7 @@ export const App = () => {
         isVisible={isEditorVisible}
         setIsVisible={setIsEditorVisible}
       />
+      {errorMessage && <div className={styles.error}>{errorMessage}</div>}
     </div>
   )
 }
